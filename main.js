16ad4c91fb58df512cdf42173f0b3875a34b7e73
Jordan Gregory <jordangregory@gmail.com>
2017-06-09 16:09:08 -0500

    added JS but need to understand it

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ var display = document.querySelector("#entry");
 console.log(display);
 var calcButtons = document.querySelectorAll(".calculatorButton");
 console.log(calcButtons);
+var expression = "";
+console.log("expression: ", expression);
 
 function updateDisplay(e) {
-  console.log(e);
+  var value = event.target.textContent;
+  console.log(value);
+  expression += value;
+  console.log("expression: ", expression);
+  display.innerHTML = expression;
 }
 
 // loop through calcButtons
 // As long a button doesn't have an id of equals, clear, or entry
 // add a "click" eventListener and pass in update display
 
-if (calcButtons === ) {
-	document.getElementsByClassName("calculatorButton").addEventListener("click", display);
-} else {
-	//do nothing
+for (var i = 0; i < calcButtons.length; i++) {
+  if (!calcButtons[i].id) {
+    calcButtons[i].addEventListener("click", updateDisplay);
+  }
 }
