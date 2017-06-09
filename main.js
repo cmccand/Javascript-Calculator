18b18ebc7c65dfc2574f1ad1f105c266b56166ec
Jordan Gregory <jordangregory@gmail.com>
2017-06-09 15:39:06 -0500

    started to add JS

---
+++ main.js
@@ -0,0 +1,18 @@
+var display = document.querySelector("#entry");
+console.log(display);
+var calcButtons = document.querySelectorAll(".calculatorButton");
+console.log(calcButtons);
+
+function updateDisplay(e) {
+  console.log(e);
+}
+
+// loop through calcButtons
+// As long a button doesn't have an id of equals, clear, or entry
+// add a "click" eventListener and pass in update display
+
+if (calcButtons === ) {
+	document.getElementsByClassName("calculatorButton").addEventListener("click", display);
+} else {
+	//do nothing
+}
