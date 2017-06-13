a28907a6e326ea070693e302c68db61ea6d2c35b
Jordan Gregory <jordangregory@gmail.com>
2017-06-13 17:00:26 -0500

    well...finished the easy mode?

--- main.js
+++ main.js
@@ -5,13 +5,38 @@ console.log(calcButtons);
 var expression = "";
 console.log("expression: ", expression);
 var operation = document.querySelectorAll(".operation");
+var state = "evaluated";
+var num1 = "";
+var operator = "";
+var num2 = "";
 
-function updateDisplay(event) {
+function handleEntry(event) {
   var value = event.target.textContent;
-  console.log(value);
-  expression += value;
-  console.log("expression: ", expression);
-  display.innerHTML = expression;
+  console.log("you clicked: ", value);
+  if (value == "C") {
+    expression = "";
+    state = "evaluated";
+  } else if (/[0-9]/.test(value) && state == "evaluated") {
+    num1 += value;
+    expression += value;
+  } else if (/[+X/\\-]/.test(value) && num1 != "" && state == "evaluated") {
+    state = "num2Entry";
+    operator = value;
+    expression += value;
+  } else if (state == "num2Entry") {
+    if (/[0-9]/.test(value)) {
+      num2 += value;
+      expression += value;
+    } else if (value == "=" && num2 != "") {
+      expression = evaluateExpresssion(
+        parseInt(num1),
+        parseInt(num2),
+        operator
+      );
+    }
+  }
+
+  display.textContent = expression;
 }
 
 // loop through calcButtons
@@ -19,7 +44,16 @@ function updateDisplay(event) {
 // add a "click" eventListener and pass in update display
 
 for (var i = 0; i < calcButtons.length; i++) {
-  if (calcButtons[i].className) {
-    calcButtons[i].addEventListener("click", updateDisplay);
+  calcButtons[i].addEventListener("click", handleEntry);
+}
+function evaluateExpresssion(n1, n2, o) {
+  if (o == "+") {
+    return n1 + n2;
+  } else if (o == "-") {
+    return n1 - n2;
+  } else if (o == "/") {
+    return n1 / n2;
+  } else if (o == "X") {
+    return n1 * n2;
   }
 }
